2e505af5bc52cfa15beca7853e896edf66f62fc4
Andrea Poli <andreapoli80@gmail.com>
2023-08-23 17:12:38 +0200

    Eliminati pezzi di struts non utilizzati

--- build.sh
+++ build.sh
@@ -2,24 +2,37 @@ RELEASE=$1
 if [ -z "${RELEASE}" ]
 then
 	echo "Tipo di build non fornito"
-	echo "Error; usage: ./build.sh snapshot|release"
+	echo "Error; usage: ./build.sh snapshot|release [upload:true/false]"
 	exit 2
 fi
 if [ ! "snapshot" == "${RELEASE}" -a ! "release" == "${RELEASE}" ]
 then
 	echo "Tipo di build '${RELEASE}' sconosciuto"
-	echo "Error; usage: ./build.sh snapshot|release"
+	echo "Error; usage: ./build.sh snapshot|release [upload:true/false]"
 	exit 3
+fi
+
+UPLOAD=$2
+if [ -z "${UPLOAD}" ]
+then
+	UPLOAD=false
+fi
+if [ ! "${UPLOAD}" == "true" -a ! "${UPLOAD}" == "false" ]
+then
+	echo "Indicazione upload non valida '${UPLOAD}'"
+	echo "Error; usage: ./build.sh snapshot|release [upload:true/false]"
+	exit 4
 fi
 
 java -fullversion > out.txt 2>&1
 JAVA_VERSION=$(cat out.txt)
-RESULT=$(cat out.txt | grep "11")
+RESULT=$(cat out.txt | grep "17")
 rm -f out.txt
 
 if [ -z "${RESULT}" ]
 then
-	echo "Richiesta java version 11, trovata: ${JAVA_VERSION}"
+	echo "Richiesta java version 17, trovata: ${JAVA_VERSION}"
+	exit 5
 fi
 
 find . -name target | xargs rm -rf
@@ -32,5 +45,12 @@ then
 	echo "Versione libreria non trovata in package/target/govway-console-ui-*.jar"
 fi
 
-GROUP_ID="org.govway-console-ui"
-bash package/script/deploy.sh package/target/govway-console-ui-${VERSION}.jar govway-console-ui ${GROUP_ID} ${VERSION} ${RELEASE}
+if [ "${UPLOAD}" == "true" ]
+then
+	echo "Upload to maven repository ..."
+
+	GROUP_ID="org.govway-console-ui"
+	bash package/script/deploy.sh package/target/govway-console-ui-${VERSION}.jar govway-console-ui ${GROUP_ID} ${VERSION} ${RELEASE}
+
+	echo "Upload to maven repository finished"
+fi
